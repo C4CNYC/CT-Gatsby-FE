40730d76c11cf57aea2ad6f48b14a1e161408540
Nural <nuro.khalafu@gmail.com>
2020-10-16 10:25:11 -0500

    #sprint1-goal3-side panel template style

--- src/templates/Units/components/Side-Panel.js
+++ src/templates/Units/components/Side-Panel.js
@@ -10,8 +10,6 @@ import TestSuite from './Test-Suite';
 import { unitTestsSelector, isUnitCompletedSelector } from '../redux';
 import { createSelector } from 'reselect';
 import './side-panel.css';
-import './custom.css';
-import './lessons.css'
 import { mathJaxScriptLoader } from '../../../utils/scriptLoaders';
 import { ReflexContainer, ReflexElement } from 'react-reflex';
 import ReactPageScroller from "react-page-scroller";
@@ -72,7 +70,7 @@ export class SidePanel extends Component {
     console.log(pageNumber)
   }
   renderSlide = (slide) => {
-    return <ReflexElement flex={1}>{ReactHtmlParser(slide.html_content)}</ReflexElement>;
+    return <div id="lesson-page"><ReflexElement flex={1} className={slide.css_class}>{ReactHtmlParser(slide.html_content)}</ReflexElement></div>;
   }
   render() {
     const {
@@ -88,34 +86,12 @@ export class SidePanel extends Component {
     } = this.props;
     return (
 
-      <ReflexContainer orientation='horizontal' className='instructions-panel' role='complementary' tabIndex='-1'>
-        <ReactPageScroller ref={c => this.reactPageScroller = c}>
+      <ReflexContainer orientation='horizontal' className='instructions-panel' role='complementary' tabIndex='-1' >
+        <ReactPageScroller ref={c => this.reactPageScroller = c} >
           {lesson_data.slides.map(this.renderSlide)}
-          {/* <ReflexElement flex={1}>
-            <UnitTitle isCompleted={isUnitCompleted}>
-              {title}
-            </UnitTitle>
-            <hr />
-          </ReflexElement>
-          <ReflexElement flex={4}>
-            <UnitDescription
-              description={description}
-              instructions={instructions}
-              section={section}
-            />
-          </ReflexElement>
-          <ReflexElement flex={2}>
-            <hr />
-            {showToolPanel && <ToolPanel guideUrl={guideUrl} videoUrl={videoUrl} />}
-            <TestSuite tests={tests} />
-          </ReflexElement> */}
         </ReactPageScroller>
       </ReflexContainer>
-      // <>
-      //   <ReactPageScroller ref={c => this.reactPageScroller = c}>
-      //     {lesson_data.slides.map(this.renderSlide)}
-      //   </ReactPageScroller>
-      // </>
+
     );
   }
 }
