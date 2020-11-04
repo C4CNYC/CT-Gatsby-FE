e5774c1ebe047f6e546686295d42b8c919db1bbb
polarbear1014 <65567444+polarbear1014@users.noreply.github.com>
2020-11-04 22:45:10 +0300

    Fix mobile drag-drop function issue

--- src/templates/Units/classic/Editor.js
+++ src/templates/Units/classic/Editor.js
@@ -2,24 +2,17 @@ import React, { Component, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+
 import {
   canFocusEditorSelector,
   executeUnit,
   inAccessibilityModeSelector,
   setEditorFocusability,
   setAccessibilityMode,
-  updateFile,
-  setMonacoEditor
+  updateFile
 } from '../redux';
 import { userSelector, isDonationModalOpenSelector } from '../../../state';
 import { Loader } from '../../../components/helpers';
-import { IconButton } from '@material-ui/core';
-import * as slider from '../components/slider_program.js';
-import * as Auth from '../components/authmanager.js';
-import $ from 'jquery';
-
-var codeconsole;
 
 const MonacoEditor = React.lazy(() => import('react-monaco-editor'));
 
@@ -35,8 +28,7 @@ const propTypes = {
   setAccessibilityMode: PropTypes.func.isRequired,
   setEditorFocusability: PropTypes.func,
   theme: PropTypes.string,
-  updateFile: PropTypes.func.isRequired,
-  setMonacoEditor: PropTypes.func.isRequired,
+  updateFile: PropTypes.func.isRequired
 };
 
 const mapStateToProps = createSelector(
@@ -55,8 +47,7 @@ const mapDispatchToProps = {
   setEditorFocusability,
   setAccessibilityMode,
   executeUnit,
-  updateFile,
-  setMonacoEditor
+  updateFile
 };
 
 const modeMap = {
@@ -96,12 +87,12 @@ const defineMonacoThemes = monaco => {
 };
 
 var lineNumberPos = [], lineNumbers, perminantData = [];
-var lineNumberElementFromID = 0, lineNumberFromID = 0, lineNumberElementToID = 0, lineNumberToID = 0, clock = 1;
+var lineNumberElementFromID = 0, lineNumberFromID = 0, lineNumberElementToID = 0, lineNumberToID = 0, clock = 1, is_mobile;
 
 class Editor extends Component {
-
   constructor(...props) {
     super(...props);
+
     this.options = {
       fontSize: '18px',
       scrollBeyondLastLine: false,
@@ -135,11 +126,9 @@ class Editor extends Component {
     this.focusOnEditor = this.focusOnEditor.bind(this);
   }
 
-  editorWillMount = monaco => {
-    defineMonacoThemes(monaco);
-  };
+  componentDidMount() {
+    is_mobile = (window.innerWidth <= 600) && (window.innerHeight <= 800);
 
-  editorDidMount = (editor, monaco) => {
     this.interval = setInterval(() => {
       // get elements data after all elements are loaded
       clock = clock - 1;
@@ -165,126 +154,252 @@ class Editor extends Component {
         lineNumberPos = pos;
         perminantData = per;
 
-        document.getElementsByClassName('margin')[0].addEventListener("mousedown", function (element) {
-          // update line number elements position data with current updated element
-          pos = [];
-          for (i = 0; i < lineNumbers.children.length; i++) {
-            var line = lineNumbers.children[i];
-            if (lineNumbers.children[i].children.length > 0) {
-              j = j + 1;
-              var position = line.children.length == 1 ? line.children[0].getBoundingClientRect() : line.children[1].getBoundingClientRect();
-              pos.push(position)
-            }
-          }
-          // init line number position array with beginning element's position
-          lineNumberPos = pos;
-
-          // get clicked element code editor ID
-          var posX = element.clientX, posY = element.clientY;
-          for (i = 0; i < lineNumberPos.length; i++) {
-            var x = lineNumberPos[i].x, y = lineNumberPos[i].y;
-            var width = lineNumberPos[i].width, height = lineNumberPos[i].height;
-            if (posX >= x && posX <= x + width && posY >= y && posY <= y + height)
-              lineNumberFromID = i;
-          }
-
-          // get clicked element line number element ID
-          j = 0;
-          for (i = 0; i < lineNumbers.children.length; i++) {
-            if (lineNumbers.children[i].children.length > 0) {
-              if (j == lineNumberFromID)
-                lineNumberElementFromID = i;
-              j = j + 1;
-            }
-          }
-        })
-
-        document.getElementsByClassName('margin')[0].addEventListener("mouseup", (element) => {
-          // get dropped element code editor ID
-          var posX = element.clientX, posY = element.clientY;
-          for (i = 0; i < lineNumberPos.length; i++) {
-            var x = lineNumberPos[i].x, y = lineNumberPos[i].y;
-            var width = lineNumberPos[i].width, height = lineNumberPos[i].height;
-            if (posX >= x && posX <= x + width && posY >= y && posY <= y + height)
-              lineNumberToID = i;
-          }
-
-          // get dropped element line number element ID
-          j = 0;
-          for (i = 0; i < lineNumbers.children.length; i++) {
-            if (lineNumbers.children[i].children.length > 0) {
-              if (j == lineNumberToID)
-                lineNumberElementToID = i;
-              j = j + 1;
-            }
-          }
-
-          // get current code's command array data
-          var codeData = this.state.currentCode;
-          var codeLines = codeData.split(/\r?\n/);
-
-          // make new code's command array after drag-drop with clicked command
-          var newCodeData = "";
-          var newPerminantData = [];
-          if (lineNumberFromID == lineNumberToID)
-            newCodeData = codeData;
-          else {
-            // make new from->to drag-droped code content and perminantData            
-            if (lineNumberFromID < lineNumberToID) {
-              for (i = 0; i < lineNumberFromID; i++) {
-                newCodeData = newCodeData + codeLines[i] + '\n';
-                newPerminantData.push(perminantData[i]);
+        // normal case
+        if (!is_mobile) {
+          document.getElementsByClassName('margin')[0].addEventListener("mousedown", function (element) {
+            // update line number elements position data with current updated element
+            pos = [];
+            for (i = 0; i < lineNumbers.children.length; i++) {
+              var line = lineNumbers.children[i];
+              if (lineNumbers.children[i].children.length > 0) {
+                j = j + 1;
+                var position = line.children.length == 1 ? line.children[0].getBoundingClientRect() : line.children[1].getBoundingClientRect();
+                pos.push(position)
               }
-              for (i = lineNumberFromID + 1; i < lineNumberToID; i++) {
-                newCodeData = newCodeData + codeLines[i] + '\n';
-                newPerminantData.push(perminantData[i]);
+            }
+            // init line number position array with beginning element's position
+            lineNumberPos = pos;
+
+            // get clicked element code editor ID
+            var posX = element.clientX, posY = element.clientY;
+            for (i = 0; i < lineNumberPos.length; i++) {
+              var x = lineNumberPos[i].x, y = lineNumberPos[i].y;
+              var width = lineNumberPos[i].width, height = lineNumberPos[i].height;
+              if (posX >= x && posX <= x + width && posY >= y && posY <= y + height)
+                lineNumberFromID = i;
+            }
+
+            // get clicked element line number element ID
+            j = 0;
+            for (i = 0; i < lineNumbers.children.length; i++) {
+              if (lineNumbers.children[i].children.length > 0) {
+                if (j == lineNumberFromID)
+                  lineNumberElementFromID = i;
+                j = j + 1;
               }
-              newCodeData = newCodeData + codeLines[lineNumberFromID] + '\n';
-              newPerminantData.push(perminantData[lineNumberFromID]);
-              for (i = lineNumberToID; i < codeLines.length; i++) {
-                if (i != codeLines.length - 1)
-                  newCodeData = newCodeData + codeLines[i] + '\n';
-                else
-                  newCodeData = newCodeData + codeLines[i];
-                newPerminantData.push(perminantData[i]);
+            }
+          })
+
+          document.getElementsByClassName('margin')[0].addEventListener("mouseup", (element) => {
+            // get dropped element code editor ID
+            var posX = element.clientX, posY = element.clientY;
+            for (i = 0; i < lineNumberPos.length; i++) {
+              var x = lineNumberPos[i].x, y = lineNumberPos[i].y;
+              var width = lineNumberPos[i].width, height = lineNumberPos[i].height;
+              if (posX >= x && posX <= x + width && posY >= y && posY <= y + height)
+                lineNumberToID = i;
+            }
+
+            // get dropped element line number element ID
+            j = 0;
+            for (i = 0; i < lineNumbers.children.length; i++) {
+              if (lineNumbers.children[i].children.length > 0) {
+                if (j == lineNumberToID)
+                  lineNumberElementToID = i;
+                j = j + 1;
               }
             }
 
+            // get current code's command array data
+            var codeData = this.state.currentCode;
+            var codeLines = codeData.split(/\r?\n/);
+
+            // make new code's command array after drag-drop with clicked command
+            var newCodeData = "";
+            var newPerminantData = [];
+            if (lineNumberFromID == lineNumberToID)
+              newCodeData = codeData;
             else {
-              for (i = 0; i < lineNumberToID; i++) {
-                newCodeData = newCodeData + codeLines[i] + '\n';
-                newPerminantData.push(perminantData[i]);
-              }
-              newCodeData = newCodeData + codeLines[lineNumberFromID] + '\n';
-              newPerminantData.push(perminantData[lineNumberFromID]);
-              for (i = lineNumberToID; i < lineNumberFromID; i++) {
-                newCodeData = newCodeData + codeLines[i] + '\n';
-                newPerminantData.push(perminantData[i]);
-              }
-              for (i = lineNumberFromID + 1; i < codeLines.length; i++) {
-                if (i != codeLines.length - 1)
+              // make new from->to drag-droped code content and perminantData            
+              if (lineNumberFromID < lineNumberToID) {
+                for (i = 0; i < lineNumberFromID; i++) {
                   newCodeData = newCodeData + codeLines[i] + '\n';
-                else
-                  newCodeData = newCodeData + codeLines[i];
-                newPerminantData.push(perminantData[i]);
+                  newPerminantData.push(perminantData[i]);
+                }
+                for (i = lineNumberFromID + 1; i <= lineNumberToID; i++) {
+                  newCodeData = newCodeData + codeLines[i] + '\n';
+                  newPerminantData.push(perminantData[i]);
+                }
+                newCodeData = newCodeData + codeLines[lineNumberFromID] + '\n';
+                newPerminantData.push(perminantData[lineNumberFromID]);
+                for (i = lineNumberToID + 1; i < codeLines.length; i++) {
+                  if (i != codeLines.length - 1)
+                    newCodeData = newCodeData + codeLines[i] + '\n';
+                  else
+                    newCodeData = newCodeData + codeLines[i];
+                  newPerminantData.push(perminantData[i]);
+                }
+              }
+
+              else {
+                for (i = 0; i <= lineNumberToID; i++) {
+                  newCodeData = newCodeData + codeLines[i] + '\n';
+                  newPerminantData.push(perminantData[i]);
+                }
+                newCodeData = newCodeData + codeLines[lineNumberFromID] + '\n';
+                newPerminantData.push(perminantData[lineNumberFromID]);
+                for (i = lineNumberToID + 1; i < lineNumberFromID; i++) {
+                  newCodeData = newCodeData + codeLines[i] + '\n';
+                  newPerminantData.push(perminantData[i]);
+                }
+                for (i = lineNumberFromID + 1; i < codeLines.length; i++) {
+                  if (i != codeLines.length - 1)
+                    newCodeData = newCodeData + codeLines[i] + '\n';
+                  else
+                    newCodeData = newCodeData + codeLines[i];
+                  newPerminantData.push(perminantData[i]);
+                }
+              }
+
+              // set upgraded content and perminantData and reload the page
+              perminantData = newPerminantData;
+              this.setState({ currentCode: newCodeData });
+              this.forceUpdate();
+            }
+          })
+        }
+
+        // mobile responsive case
+        else {
+          document.getElementsByClassName('margin')[0].addEventListener("touchstart", function (element) {
+            // update line number elements position data with current updated element
+            pos = [];
+            for (i = 0; i < lineNumbers.children.length; i++) {
+              var line = lineNumbers.children[i];
+              if (lineNumbers.children[i].children.length > 0) {
+                j = j + 1;
+                var position = line.children.length == 1 ? line.children[0].getBoundingClientRect() : line.children[1].getBoundingClientRect();
+                pos.push(position)
+              }
+            }
+            // init line number position array with beginning element's position
+            lineNumberPos = pos;
+
+            // get clicked element code editor ID
+            var posX = element.changedTouches[0].clientX, posY = element.changedTouches[0].clientY;
+            for (i = 0; i < lineNumberPos.length; i++) {
+              var x = lineNumberPos[i].x, y = lineNumberPos[i].y;
+              var width = lineNumberPos[i].width, height = lineNumberPos[i].height;
+              if (posX >= x && posX <= x + width && posY >= y && posY <= y + height)
+                lineNumberFromID = i;
+            }
+
+            // get clicked element line number element ID
+            j = 0;
+            for (i = 0; i < lineNumbers.children.length; i++) {
+              if (lineNumbers.children[i].children.length > 0) {
+                if (j == lineNumberFromID)
+                  lineNumberElementFromID = i;
+                j = j + 1;
+              }
+            }
+          })
+
+          document.getElementsByClassName('margin')[0].addEventListener("touchend", (element) => {
+            // get dropped element code editor ID
+            var posX = element.changedTouches[0].clientX, posY = element.changedTouches[0].clientY;
+            for (i = 0; i < lineNumberPos.length; i++) {
+              var x = lineNumberPos[i].x, y = lineNumberPos[i].y;
+              var width = lineNumberPos[i].width, height = lineNumberPos[i].height;
+              if (posX >= x && posX <= x + width && posY >= y && posY <= y + height)
+                lineNumberToID = i;
+            }
+
+            // get dropped element line number element ID
+            j = 0;
+            for (i = 0; i < lineNumbers.children.length; i++) {
+              if (lineNumbers.children[i].children.length > 0) {
+                if (j == lineNumberToID)
+                  lineNumberElementToID = i;
+                j = j + 1;
               }
             }
 
-            // set upgraded content and perminantData and reload the page
-            perminantData = newPerminantData;
-            this.setState({ currentCode: newCodeData });
-            this.forceUpdate();
-          }
-        })
+            // get current code's command array data
+            var codeData = this.state.currentCode;
+            var codeLines = codeData.split(/\r?\n/);
+
+            // make new code's command array after drag-drop with clicked command
+            var newCodeData = "";
+            var newPerminantData = [];
+            if (lineNumberFromID == lineNumberToID)
+              newCodeData = codeData;
+            else {
+              // make new from->to drag-droped code content and perminantData            
+              if (lineNumberFromID < lineNumberToID) {
+                for (i = 0; i < lineNumberFromID; i++) {
+                  newCodeData = newCodeData + codeLines[i] + '\n';
+                  newPerminantData.push(perminantData[i]);
+                }
+                for (i = lineNumberFromID + 1; i <= lineNumberToID; i++) {
+                  newCodeData = newCodeData + codeLines[i] + '\n';
+                  newPerminantData.push(perminantData[i]);
+                }
+                newCodeData = newCodeData + codeLines[lineNumberFromID] + '\n';
+                newPerminantData.push(perminantData[lineNumberFromID]);
+                for (i = lineNumberToID + 1; i < codeLines.length; i++) {
+                  if (i != codeLines.length - 1)
+                    newCodeData = newCodeData + codeLines[i] + '\n';
+                  else
+                    newCodeData = newCodeData + codeLines[i];
+                  newPerminantData.push(perminantData[i]);
+                }
+              }
+
+              else {
+                for (i = 0; i <= lineNumberToID; i++) {
+                  newCodeData = newCodeData + codeLines[i] + '\n';
+                  newPerminantData.push(perminantData[i]);
+                }
+                newCodeData = newCodeData + codeLines[lineNumberFromID] + '\n';
+                newPerminantData.push(perminantData[lineNumberFromID]);
+                for (i = lineNumberToID + 1; i < lineNumberFromID; i++) {
+                  newCodeData = newCodeData + codeLines[i] + '\n';
+                  newPerminantData.push(perminantData[i]);
+                }
+                for (i = lineNumberFromID + 1; i < codeLines.length; i++) {
+                  if (i != codeLines.length - 1)
+                    newCodeData = newCodeData + codeLines[i] + '\n';
+                  else
+                    newCodeData = newCodeData + codeLines[i];
+                  newPerminantData.push(perminantData[i]);
+                }
+              }
+
+              // set upgraded content and perminantData and reload the page
+              perminantData = newPerminantData;
+              this.setState({ currentCode: newCodeData });
+              this.forceUpdate();
+            }
+          })
+        }
+
 
         clearInterval(this.interval)
       }
     }, 1000);
-    const { setMonacoEditor } = this.props;
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  editorWillMount = monaco => {
+    defineMonacoThemes(monaco);
+  };
+
+  editorDidMount = (editor, monaco) => {
     this._editor = editor;
-
-    setMonacoEditor(editor)
-
     this._editor.updateOptions({
       accessibilitySupport: this.props.inAccessibilityMode ? 'on' : 'auto'
     });
@@ -338,16 +453,7 @@ class Editor extends Component {
         this.props.setAccessibilityMode(true);
       }
     });
-    Auth.getCode((codes) => {
-      if (typeof codes != 'null' && typeof codes != 'undefined') {
-        this._editor.setValue(codes);
-      }
-    });
   };
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
 
   focusOnHotkeys() {
     if (this.props.containerRef.current) {
@@ -362,9 +468,10 @@ class Editor extends Component {
   onChange = editorValue => {
     const { updateFile, fileKey } = this.props;
     updateFile({ key: fileKey, editorValue });
-    // this.props.executeUnit();
-    slider.validate_function(editorValue);
-    Auth.savCode(editorValue);
+
+    this.props.executeUnit();
+
+    this.setState({ currentCode: editorValue })
   };
 
   componentDidUpdate(prevProps) {
@@ -385,8 +492,8 @@ class Editor extends Component {
           language={modeMap[ext]}
           onChange={this.onChange}
           options={this.options}
-          value={this.state.currentCode}
           theme={editorTheme}
+          value={this.state.currentCode}
           automaticLayout={true}
           height="100%"
         />
